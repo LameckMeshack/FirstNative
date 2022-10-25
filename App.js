3de8fc2184a196a76de2a1b8b8433db0f96f855a
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-25 18:27:37 +0300

    *Added fonts from assest

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { useFont } from "expo-font";
+import { useFonts } from "expo-font";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 
@@ -16,7 +16,13 @@ const theme = {
 };
 
 export default function App() {
-  const [loaded] = useFont({});
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
